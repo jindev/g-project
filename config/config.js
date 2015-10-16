b0d5c5d479fe31cf8d6fd3e4a28ff62db015b387
jindev <bjkim.dev@gmail.com>
2015-10-17 01:44:48 +0900

    config

---
+++ config/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _ = require('lodash'),
+    glob = require('glob');
+
+
+module.exports = _.extend(
+    require('./env/all'),
+    require('./env' + process.env.NODE_ENV) || {}
+);
---
+++ config/env/all.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    app : {
+        title : 'gab-project',
+        description : '',
+        keyword : ''
+    },
+    port : process.env.prot || 3000,
+    templateEngine : 'ejs',
+    sessionCollection : 'sessions'
+};
